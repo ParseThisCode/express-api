2cb86f5d00c050b3ec33dadae55c1342e0dda55f
ParseThisCode <kamilochman79@gmail.com>
2022-07-25 21:54:59 +0200

    port fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { createServer } from "http";
 import { middleware } from "./server/middleware.js";
 
 const server = createServer(middleware);
+const PORT = process.env.PORT || 8000;
 
-server.listen(8000, () => 
- console.log("http://localhost:8000/"))
+server.listen(PORT, () =>
+ console.log(`http://localhost:${PORT}/`))
